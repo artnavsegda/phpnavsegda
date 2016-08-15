98a57d1e75db020b76147666829f87647ec9deb8
Art Navsegda <artnavsegda@gmail.com>
2016-08-15 05:38:57 +0300

    pew pew

--- modbus.php
+++ modbus.php
@@ -7,4 +7,26 @@
  */
 require_once dirname(__FILE__) . '/Phpmodbus/ModbusMaster.php';
 
+$modbus = new ModbusMaster($_REQUEST["host"], "TCP");
+
+if ($_REQUEST["registertype"] == "coil")
+{
+    // nothing
+}
+
+switch ($_REQUEST["registertype"])
+{
+    case "coil":
+        $coilData = $modbus->readCoils(0, 0, 5);
+        break;
+    case "discrete":
+        break;
+    case "inputregister":
+        break;
+    case "holdingregister":
+        break;
+    default:
+        break;
+}
+
 ?>
