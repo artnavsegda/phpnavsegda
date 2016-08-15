04a2a6eaf8d587d2d8d3eb61f50cc574289ff5ad
Art Navsegda <artnavsegda@gmail.com>
2016-08-15 05:24:09 +0300

    pew pew

--- hello.php
+++ hello.php
@@ -1,6 +1,11 @@
 <?php
-echo var_export($_SERVER);
+// echo var_export($_SERVER);
 echo var_export($_REQUEST);
-echo var_export($_GET);
-echo var_export($_POST);
+//echo var_export($_GET);
+//echo var_export($_POST);
+//echo $_POST["host"];
+echo $_REQUEST["host"];
+echo $_REQUEST["register"];
+echo $_REQUEST["registertype"];
+echo $_REQUEST["contents"];
 ?>
