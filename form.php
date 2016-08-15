48cf796ee3828a83421a352e529cede02623f798
Art Navsegda <artnavsegda@gmail.com>
2016-08-15 04:59:31 +0300

    pew pew

--- form.php
+++ form.php
@@ -8,13 +8,13 @@
 <p>
 <form action="hello.php" method="get">
     Host/IP addr:
-    <input type="text" name="port" value="127.0.0.1">
+    <input type="text" name="host" value="127.0.0.1">
     <br>
     Register:
-    <input type="number" name="register" min="1" max="5">
+    <input type="number" name="register" min="1" max="5" value="3">
     <br>
     Type:
-    <select name="state2">
+    <select name="registertype">
         <option value="coil">Coil</option>
         <option value="discrete">Discrete Input</option>
         <option value="inputregister">Input Register</option>
@@ -22,7 +22,7 @@
     </select>
     <br>
     Value:
-    <input type="number" name="value" min="1" max="5">
+    <input type="number" name="contents" min="1" max="5" value="4">
     <br>
     <input type="submit" value="Submit">
 </form>
