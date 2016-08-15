5babd5ace705510d9f4cc1f652596a6c23406a32
Art Navsegda <artnavsegda@gmail.com>
2016-08-15 13:00:10 +0300

    pretty fly

--- editmodbusengine.php
+++ editmodbusengine.php
@@ -11,12 +11,6 @@ try
         case "coil":
             $recData = $modbus->readCoils(0, $_REQUEST["registeraddr"], 1);
             break;
-        case "discrete":
-            $recData = $modbus->readInputDiscretes(0, $_REQUEST["registeraddr"], 1);
-            break;
-        case "inputregister":
-            $recData = $modbus->readMultipleInputRegisters(0, $_REQUEST["registeraddr"], 1);
-            break;
         case "holdingregister":
             $recData = $modbus->readMultipleRegisters(0, $_REQUEST["registeraddr"], 1);
             break;
@@ -31,16 +25,6 @@ catch (Exception $e)
     echo $e;
     exit;
 }
-
-// Print status information
-//echo "</br>Status:</br>" . $modbus;
-
-// Print read data
-//echo "</br>Data:</br>";
-//var_dump($recData);
-//echo "</br>Value:</br>";
-//var_dump(PhpType::bytes2signedInt($recData));
-//echo "</br>";
 
 ?>
 
