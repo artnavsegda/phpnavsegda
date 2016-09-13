9f97f710128b90981b80d5cb72e1fe316f14b05c
Art Navsegda <artnavsegda@gmail.com>
2016-09-13 16:55:20 +0300

    adressing issue

--- settingsengine.php
+++ settingsengine.php
@@ -24,8 +24,8 @@ try
     $macadress[0] = $_REQUEST["mac5"];
     $macadress[1] = $_REQUEST["mac6"];
     $modbus->writeSingleRegister(0, 104, $macadress, array("INT"));
-    $modbus->writeSingleCoil(0, 100, array(TRUE));
-    $modbus->writeSingleCoil(0, 101, array(TRUE));
+    $modbus->writeSingleCoil(0, 1100, array(TRUE));
+    $modbus->writeSingleCoil(0, 1101, array(TRUE));
 }
 catch (Exception $e)
 {
